cdfe10006d55d841ab946c06c6eb19c04bee459d
Nathan Firmo <nathanpalmares@uol.com.br>
2020-01-24 22:46:12 -0300

    Dividindo em componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const MostraVoltas = (props) => {
+  return (
+      <p>
+      {props.voltas}<br />
+      Voltas
+    </p>    
+  )
+}
+
+const MostraTempo = (props) => {
+  return (
+    <p>
+    {props.tempo}<br />
+    Tempo médio por volta
+    </p>    
+  )
+}
+
+const Button = (props) => <button>{props.text}</button>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MostraVoltas voltas='15'/>
+      <Button text='+' />
+      <Button text='-' />
+      <MostraTempo tempo='01:35' />
+      <Button text='Iniciar' />
+      <Button text='Reiniciar' />
     </div>
   );
 }
